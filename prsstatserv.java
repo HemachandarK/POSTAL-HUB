18f56e25462096ee6f2d61f4f0dba1485979aefe
HemachandarK <138129846+HemachandarK@users.noreply.github.com>
2023-12-21 17:11:15 +0530

    Update prsstatserv.java

--- prsstatserv.java
+++ prsstatserv.java
@@ -41,11 +41,11 @@ public class prsstatserv extends HttpServlet {
 		Connection connection=null;
 		Statement st=null;
 		
-		String host="localhost";
+		String host="postal.postgres.database.azure.com";
 		String port="5432";
-		String db_name="post";
-		String username="postgres";
-		String password="postgres";
+		String db_name="postgres";
+		String username="post";
+		String password="Phub@2023";
 		
 		try {
 			
