5b30a69626d05f91ae90dd9f2228caf9c86f14e7
HemachandarK <138129846+HemachandarK@users.noreply.github.com>
2023-12-21 17:12:30 +0530

    Update transerv.java

--- transerv.java
+++ transerv.java
@@ -39,11 +39,11 @@ public class transerv extends HttpServlet {
 		Connection connection=null;
 		Statement st=null;
 		Statement st3=null;
-		String host="localhost";
+		String host="postal.postgres.database.azure.com";
 		String port="5432";
-		String db_name="post";
-		String username="postgres";
-		String password="postgres";
+		String db_name="postgres";
+		String username="post";
+		String password="Phub@2023";
 		
 		try {
 			
