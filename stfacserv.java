6584a6f398d0bd14247b927ec50539d15736b4b8
HemachandarK <138129846+HemachandarK@users.noreply.github.com>
2023-12-21 17:12:05 +0530

    Update stfacserv.java

--- stfacserv.java
+++ stfacserv.java
@@ -41,11 +41,11 @@ public class stfacserv extends HttpServlet {
 		Connection connection=null;
 		java.sql.Statement st=null;
 		java.sql.Statement st1=null;
-		String host="localhost";
+		String host="postal.postgres.database.azure.com";
 		String port="5432";
-		String db_name="post";
-		String username="postgres";
-		String password="postgres";
+		String db_name="postgres";
+		String username="post";
+		String password="Phub@2023";
 		
 try {
 			
